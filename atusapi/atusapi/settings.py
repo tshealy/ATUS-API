63990af1cf5a1dc536c47852d3af10186e335911
Keith Calise <KeithCalise@gmail.com>
2015-06-27 11:49:42 -0400

    added pagination

--- atusapi/atusapi/settings.py
+++ atusapi/atusapi/settings.py
@@ -110,3 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
+
