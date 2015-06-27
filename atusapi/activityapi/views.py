a8ec48b39fda4fdd14458a7a86e1a9b10b4af54a
sovello <sovellohpmgani@gmail.com>
2015-06-27 10:20:24 -0400

    modifies the urls.py to accommodate the new change in previous commit

---
+++ atusapi/activityapi/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics, filters
+from respondents.models import People, Respondents, HouseholdList, Activity, ActivityList
+from rest_framework.exceptions import PermissionDenied
+import django_filters
+from django.contrib.auth.models import User
+from .serializers import ActivitySerializer, ActivityListSerializer
+
+
+    
+class ActivityListDetailView(generics.RetrieveAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    serializer_class = ActivityListSerializer
+    queryset = ActivityList.objects.all()
+
+
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset= ActivityList.objects.all()
+    serializer_class = ActivityListSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
--- atusapi/atusapi/urls.py
+++ atusapi/atusapi/urls.py
@@ -16,21 +16,21 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from api import views as api_views
+from activityapi import views as activityapi_views
 from django.views.generic import TemplateView, ListView
 from django.contrib.auth.decorators import login_required
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r"respondents", api_views.RespondentsViewSet, base_name='respondents')
-router.register(r"activity", api_views.ActivityViewSet)
+router.register(r"activity", activityapi_views.ActivityViewSet)
 router.register(r"person", api_views.PeopleViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace ='rest_framework')),
-    # url(r'^respondents/(?P<pk>\d+)/activity-list/$', views.)
-    # url(r'^respondents/activity-list')
+    url(r'^api-auth/', include('rest_framework.urls', namespace ='rest_framework')),  
+    url(r'^activity/(?P<pk>\d+)$', activityapi_views.ActivityListDetailView.as_view(), name='activitylist-detail'),
     url(r'^householdlist/$', api_views.HouseholdListView.as_view(), name='householdlist'),
     url(r'^householdlist/(?P<pk>\d+)/$', api_views.HouseholdListDetailView.as_view(), name='householdlist-detail'),
 ]
